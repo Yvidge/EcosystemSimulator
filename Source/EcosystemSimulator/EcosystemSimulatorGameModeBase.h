2fe666b6bc4aed520301f4ea718631bc3cf3a508
Andrew <64382796+HInquisition@users.noreply.github.com>
2022-02-20 22:45:07 +0300

    Init project files

---
+++ Source/EcosystemSimulator/EcosystemSimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EcosystemSimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECOSYSTEMSIMULATOR_API AEcosystemSimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
